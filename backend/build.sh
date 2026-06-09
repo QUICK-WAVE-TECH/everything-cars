9d39d58036833d39b37b08d626be859cbfa036e5
Nnamdi <namy_park@protonmail.com>
2026-06-09 01:32:35 +0100

    fix: ensure collectstatic runs with production settings, add WHITENOISE_MANIFEST_STRICT=False

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,8 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+# collectstatic with production settings
+export DJANGO_SETTINGS_MODULE=config.settings.production
 python manage.py collectstatic --noinput
 python manage.py migrate
--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -136,6 +136,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+WHITENOISE_MANIFEST_STRICT = False
 
 # Logging
 LOGGING = {
